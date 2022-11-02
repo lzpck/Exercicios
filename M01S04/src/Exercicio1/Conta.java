dc8cc116d9c8db37e39b6580742d6914bc6f3f87
lzpck <leandro-zep@live.com>
2022-11-02 15:07:02 -0300

    [M01S04]

---
+++ M01S04/src/Exercicio1/Conta.java
@@ -0,0 +1,8 @@
+package Exercicio1;
+
+public abstract class Conta {
+
+    private double saldo;
+
+    protected abstract double ObterSaldoAtual();
+}
---
+++ M01S04/src/Exercicio1/Operavel.java
@@ -0,0 +1,8 @@
+package Exercicio1;
+
+public interface Operavel {
+
+    public void depositar(double valor);
+
+    public double sacar (double valor);
+}
