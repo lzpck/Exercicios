7e4d1492e3648b7008532715e71c942ef322531a
lzpck <leandro-zep@live.com>
2023-01-27 08:33:23 -0300

    [M02S02]

---
+++ M02S02/Exercicio3.js
@@ -0,0 +1,5 @@
+let num1 = parseFloat(prompt("Digite um número:"));
+let num2 = parseFloat(prompt("Digite outro número:"));
+let result = num1 + num2;
+
+document.getElementById("result").innerHTML = "A soma dos números é: " + result;
