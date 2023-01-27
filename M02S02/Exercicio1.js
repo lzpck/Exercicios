19ccdcfab5a6489ca0a71ef6b22770957b8fbd67
lzpck <leandro-zep@live.com>
2023-01-27 08:27:21 -0300

    [M02S02]

--- M02S02/Exercicio1.js
+++ M02S02/Exercicio1.js
@@ -1,3 +1,4 @@
-var name = prompt("Por favor, digite seu nome:");
-console.log("Seu nome é: " + name);
+let name = prompt("Qual é o seu nome?");
+let email = prompt("Qual é o seu email?");
 
+document.getElementById("info").innerHTML = "<h4>Nome: " + name + "</h4><h4>Email: " + email + "</h4>";
