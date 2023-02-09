6ebac31b829568435f385374aeb203d4f2aa5cf2
lzpck <leandro-zep@live.com>
2023-02-09 20:29:31 -0300

    [M02S05]

---
+++ M02S04/Exercicio07/script.js
@@ -0,0 +1,33 @@
+const jogadoresSelecao94 = [{
+        nome: 'Taffarel',
+        posicao: 'Goleiro',
+    },
+    {
+        nome: 'Jorginho',
+        posicao: 'Lateral Direito',
+    },
+    {
+        nome: 'Ricardo Rocha',
+        posicao: 'Zagueiro',
+    },
+    {
+        nome: 'Mauro Silva',
+        posicao: 'Volante',
+    },
+    {
+        nome: 'Bebeto',
+        posicao: 'Atacante',
+    },
+    {
+        nome: 'Romário',
+        posicao: 'Atacante',
+    },
+    {
+        nome: 'Dunga',
+        posicao: 'Volante',
+    },
+];
+
+const nomesJogadores = jogadoresSelecao94.map(jogador => jogador.nome);
+
+console.log(nomesJogadores);
