ccdf607b0f639d413fd86c5fe22e787050661fc5
lzpck <leandro-zep@live.com>
2023-03-05 20:39:32 -0300

    [M02S05]

---
+++ M02S05/Exercicio08.js
@@ -0,0 +1,29 @@
+function fakeAPIFilmes(idRequisição) {
+    const filmes = {
+        1: "matrix",
+        2: "O poderoso Chefão",
+        3: "A espera de um milagre",
+        4: "O clube da luta",
+        5: "Coração valente",
+        6: "O rei leão",
+        7: "Forrest Gump - O Contador de Histórias",
+        8: "Os bons companheiros",
+        9: "O irlândes",
+        10: "Um sonho de liberdade",
+    };
+
+    const tempoDeResposta = Math.random() * 1000;
+    return new Promise(function (sucesso) {
+        setTimeout(() => sucesso(filmes[idRequisição]), tempoDeResposta)
+    });
+}
+
+const idsRequisicao = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+Promise.all(idsRequisicao.map(id => fakeAPIFilmes(id)))
+    .then(respostas => {
+        respostas.forEach((resposta, indice) => {
+            console.log(`Requisição ${indice+1}: ${resposta}`);
+        });
+    })
+    .catch(erro => console.error(erro));
